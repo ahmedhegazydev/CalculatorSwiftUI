3d84b7f296df6537717254d59d6881a59b480aae
Ahmed Hegazy <wowrar1234@gmail.com>
2020-02-10 00:08:05 +0200

    v h z stacks

--- CalculatorIosSwiftUi/ContentView.swift
+++ CalculatorIosSwiftUi/ContentView.swift
@@ -10,10 +10,54 @@ import SwiftUI
 
 struct ContentView: View {
     
-    
+    let charachers = [
+        ["7", "8", "9", "X"],
+        ["7", "8", "9", "X"],
+        ["7", "8", "9", "X"]
+    ]
     
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack(alignment: .bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                //            HStack{
+                //                Text("0").font(.system(size: 20))
+                //                Text("0").font(.system(size: 20))
+                //                Text("0").font(.system(size: 20))
+                //                Text("0").font(.system(size: 20))
+                //            }
+                HStack{
+                    Spacer()
+                    Text("Res").foregroundColor(.white)
+                        .font(.system(size: 30))
+                }.padding()
+                
+                VStack{
+                    ForEach(charachers, id: \.self){ row in
+                        HStack{
+                            //                    ForEach(["7", "8", "9", "X"], id: \.self) { button in
+                            //                        Text(button)
+                            //                            .font(.system(size: 30))
+                            //                            .frame(width: 50, height: 50)
+                            //                    }
+                            ForEach(row, id: \.self) { button in
+                                Text(button)
+                                    .font(.system(size: 30))
+                                    .frame(width: 80, height: 80)
+                                    .foregroundColor(.white)
+                                
+                            }
+                            
+                        }
+                    }
+                }
+                
+            }
+        }
+        
+        
     }
 }
 
