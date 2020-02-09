f7f7b94c2e0f02e4f4279997059f94d7f6e29502
Ahmed Hegazy <wowrar1234@gmail.com>
2020-02-09 23:29:50 +0200

    Initial Commit

---
+++ CalculatorIosSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorIosSwiftUi
+//
+//  Created by Ahmed Hegazy on 2/9/20.
+//  Copyright © 2020 Ahmed Hegazy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
